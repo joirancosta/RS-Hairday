d696a05d4b236b1c5cb3483ae4c3c0bf8a3e5cfa
“Joiran <joiran.costa.jc@gmail.com>
2025-06-14 20:48:53 -0300

    rederizando agendamento

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,4 +1,5 @@
 import { scheduleFetchByDay } from "../../services/shedule-fetch-by-day.js";
+import { schedulesShow } from "../schedules/show.js";
 import { hoursLoad } from "../form/hours-load.js";
 
 // Seleciona o input de data
@@ -10,7 +11,9 @@ export async function schedulesDay() {
   const date = selectedDate.value
   // Busca na API os agendamentos
   const dailySchedules = await scheduleFetchByDay({date})
-  console.log(dailySchedules)
+  // console.log(dailySchedules)
+  // Exibe os agendamentos
+  schedulesShow({dailySchedules})
   // Renderiza as horas disponíveis
   hoursLoad({date})
   // Os horários disponíveis (horário futuro + não agendado) do lado esquerdo (form)
